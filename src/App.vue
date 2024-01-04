3552ba0245c062f6c30f66b30122a40154b098e3
danelian <141317269+danelian@users.noreply.github.com>
2024-01-05 00:05:03 +0400

    localStorage
    fetch from
    save to
    onMounted

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useToast } from 'vue-toastification'
 
 import Header from './components/Header.vue'
@@ -10,12 +10,15 @@ import AddTransaction from './components/AddTransaction.vue'
 
 const toast = useToast()
 
-const transactions = ref([
-  { id: 1, text: "Flower", amount: -19.99 },
-  { id: 2, text: "Salary", amount: 299.97 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-]);
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transaction'))
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions
+  }
+})
 
 // Get total
 const total = computed(() => {
@@ -52,6 +55,8 @@ const handleTransactionSubmitted = (transactionData) => {
     amount: transactionData.amount
   })
 
+  saveTransactionsToLocalStorage()
+
   toast.success('Transaction added')
 }
 
@@ -66,7 +71,14 @@ const handleTransactionDeleted = (id) => {
     (transaction) => transaction.id !== id
   )
 
+  saveTransactionsToLocalStorage()
+
   toast.success('Transaction deleted')
+}
+
+// Save to localStorage
+const saveTransactionsToLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
 }
 </script>
 
